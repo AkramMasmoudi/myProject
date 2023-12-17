e4bd6110c046323d8f5baf02d3ade5b3d3537048
Akram <masmoudiakram14@gmail.com>
2023-12-17 08:54:30 +0100

    17-12-2023

--- src/main/java/com/akram/myProject/servicesImpl/PersonServiceImpl.java
+++ src/main/java/com/akram/myProject/servicesImpl/PersonServiceImpl.java
@@ -8,6 +8,7 @@ import com.akram.myProject.services.PersonService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -39,6 +40,7 @@ public class PersonServiceImpl implements PersonService {
     }
 
     @Override
+    @Transactional
     public boolean deletePerson(Long personId) {
         try {
             this.personRepository.deletePersonByPersonId(personId);
